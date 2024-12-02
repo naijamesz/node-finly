9f460fe48e035dde4c95d36a613fe080a373dcf3
naijamesz <kodlikejamesz@hotmail.com>
2024-12-02 16:18:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import http from 'http';
+const server = http.createServer((req, res) => {
+  const { url } = req;
+  console.log(url);
+  if (url === '/') {
+    res.end('Hello From Node.js');
+  } else if (url === '/contact') {
+    res.end('The Contact Page');
+  } else if (url === '/about') {
+    res.end('The About Page');
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
+});
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
